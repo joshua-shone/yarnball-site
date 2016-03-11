b3239497a44c7f08d17044c365f65def203579b4
Joshua Shone <josh.shone@gmail.com>
2016-03-11 19:33:49 +0100

    building now optional, assets/ bower../ serves all
    - The site can now be served directly from the source directory for development.
    - All assets (except for index.html) now kept under assets/ and bower_components/, which avoids conflicts with usernames.
    - Simplified nginx_dev.conf, now routes any unhandled paths to index.html

--- gulpfile.js
+++ gulpfile.js
@@ -12,70 +12,76 @@ var svgmin   = require('gulp-svgmin');
 
 // Utilities
 var gulp_if = require('gulp-if');
-var yargs   = require('yargs');
 
 // Dependencies
 // var bowerFiles = require('main-bower-files');
 
 gulp.task('static', [
-  'static-bower',
+  'static-index',
   'static-html',
   'static-js',
   'static-sass',
   'static-components-sass',
   'static-png',
   'static-svg',
+  'static-bower',
 ]);
 
+gulp.task('static-index', function() {
+  return gulp.src('index.html')
+//     .pipe(htmlmin({collapseWhitespace: true, minifyJS: true}))
+    .pipe(gulp.dest('dist/'));
+});
+
 gulp.task('static-html', function() {
-  return gulp.src('static/**/*.html')
-//     .pipe(gulp_if(yargs.argv.production, htmlmin({collapseWhitespace: true, minifyJS: true})))
-    .pipe(gulp.dest('dist/static/'))
+  return gulp.src('assets/**/*.html')
+//     .pipe(htmlmin({collapseWhitespace: true, minifyJS: true}))
+    .pipe(gulp.dest('dist/assets/'))
 //     .pipe(browserSync.stream());
 });
 
 gulp.task('static-js', function() {
-  return gulp.src('static/**/*.js')
-    .pipe(gulp_if(yargs.argv.production, uglifyjs()))
-    .pipe(gulp.dest('dist/static/'))
+  return gulp.src('assets/**/*.js')
+    .pipe(uglifyjs())
+    .pipe(gulp.dest('dist/assets/'))
 //     .pipe(browserSync.stream());
 });
 
 gulp.task('static-sass', function() {
-  return gulp.src('static/*.sass')
+  return gulp.src('assets/*.sass')
     .pipe(gulp_if('!_*.sass', sass()))
-    .pipe(gulp_if(yargs.argv.production, cssnano()))
-    .pipe(gulp.dest('dist/static/'))
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/assets/'))
 //     .pipe(browserSync.stream());
 });
 
 gulp.task('static-components-sass', function() {
-  return gulp.src('static/*/*.sass')
+  return gulp.src('assets/*/*.sass')
     .pipe(gulp_if('!_*.sass', sass()))
-    .pipe(gulp_if(yargs.argv.production, cssnano()))
+    .pipe(cssnano())
     .pipe(styleModule())
-    .pipe(gulp.dest('dist/static/'))
+    .pipe(gulp.dest('dist/assets/'))
 //     .pipe(browserSync.stream());
 });
 
 gulp.task('static-png', function() {
-  return gulp.src('static/**/*.png')
-    .pipe(gulp.dest('dist/static/'))
+  return gulp.src('assets/**/*.png')
+    .pipe(gulp.dest('dist/assets/'))
 //     .pipe(browserSync.stream());
 });
 
 gulp.task('static-svg', function() {
-  return gulp.src('static/**/*.svg')
-    .pipe(gulp_if(yargs.argv.production, svgmin()))
-    .pipe(gulp.dest('dist/static/'))
+  return gulp.src('assets/**/*.svg')
+    .pipe(svgmin())
+    .pipe(gulp.dest('dist/assets/'))
 //     .pipe(browserSync.stream());
 });
 
 gulp.task('static-bower', function() {
 //   gulp.src(bowerFiles())
   return gulp.src('bower_components/**/*')
-    .pipe(gulp_if(yargs.argv.production, gulp_if('*.js', uglifyjs())))
+//     .pipe(gulp_if('*.js', uglifyjs()))
 //     .pipe(gulp_if('*.html', htmlmin()))
-    .pipe(gulp_if(yargs.argv.production, gulp_if('*.css', cssnano())))
-    .pipe(gulp.dest('dist/static/bower_components/'));
+    .pipe(gulp_if('*.css', cssnano()))
+    .pipe(gulp.dest('dist/bower_components/'));
 });
